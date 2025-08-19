a995df37ef971c5c922c4e36935fc1dfa64dabba
Chris <157814013+cbonnin88@users.noreply.github.com>
2025-08-19 14:47:36 +0200

    Delete compensation_analysis_queries directory

--- compensation_analysis_queries/average salary by job title.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-  job_title,
-  ROUND(AVG(base_salary)) AS average_salary
-FROM 
-  `compensation-analysis.employee_data.compensation`
-GROUP BY
-  1;
--- compensation_analysis_queries/top 5 highest-paid employees.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-  employee_id,
-  base_salary
-FROM 
-  `compensation-analysis.employee_data.compensation`
-ORDER BY 
-  base_salary DESC
-LIMIT 5
